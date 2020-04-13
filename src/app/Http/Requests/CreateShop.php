2dd2a1f6ffd92abbb4e472a23516bdb1ee703455
suzukiyoship <suzukiyoship@systena.co.jp>
2020-04-13 16:24:08 +0900

    handle validate localization

--- src/app/Http/Requests/CreateShop.php
+++ src/app/Http/Requests/CreateShop.php
@@ -28,4 +28,12 @@ class CreateShop extends FormRequest
             'rate' => ['required', 'regex:/^([1-9][0-9]{0,1}|0)(\.[0-9]{1})?$/'],
         ];
     }
+
+    public function attributes()
+    {
+        return [
+            'name' => '名前',
+            'rate' => 'レート',
+        ];
+    }
 }
